43e4e6b5d2a731d221cb03812f50e2b7e6a38093
gchicaduque <gchicaduque@gmail.com>
2018-02-14 00:03:00 -0500

    Cambios cypress

--- Cypress/buscar_profesor.js
+++ Cypress/buscar_profesor.js
@@ -3,8 +3,8 @@ describe('Los estudiantes login', function() {
         cy.visit('https://losestudiantes.co')
 	cy.contains('Cerrar').click()
 	//Lineas nuevas  
-      	cy.get('.Select-control').click()
-	cy.get('.Select-input').type('mario linares')
+    cy.get('.Select-control').click()
+	cy.get('input[role=combobox]').type('mario linares')
 	//cy.get('.Select-control').select('Mario Linares Vasquez - Ingeniería de Sistemas')
     })
 })
